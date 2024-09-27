1825effc7921be2daa04ada86b4c948a1f8aa26b
Alain Wafflard <alain.wafflard@gmail.com>
2024-09-27 19:04:00 +0200

    start2

--- git_class/www_mvc/app/config/app.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const ROOT_DIR = "../app/";
-
-const AJAX_ENABLED = true;   // false;
-
--- git_class/www_mvc/external/finance_lib.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * Ici se trouverait le code des bibliothèques externes,
- * téléchargées du web, reçues d'un partenaire, etc.
- *
- * Interdiction de modifier ce code => permettre les mises à jour
- */
-
